9de3cbb7fe188977a7c65dfd9cc9a2d69ac04bcb
JAM789 <112285733+JAM789-lab@users.noreply.github.com>
2023-12-02 18:18:57 -0500

    Added the class for Student and Educator
    Some of the methods we have outlined cannot be implemented yet so they have been left commented out.

---
+++ Educator.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Educator{
+    String username; // Educator's username
+    String password; // Educator's password
+    ArrayList<Student> roster; // Educator's roster (List of Students)
+    ArrayList<CodingChallenge> challenges; // Educator's challenges (List of CodingChallenges)
+
+    public Educator(String name){
+        username = name;
+        password = "password"; // We don't need anything fancy for login details so password will just be this by default
+
+        // These lists will stay empty by default so we can keep better track of everything we add in
+        roster = new ArrayList<Student>();
+        challenges = new ArrayList<CodingChallenge>();
+    }
+
+    // Adds a Student to the Educator's roster
+    public void addStudent(Student s){
+        roster.add(s);
+    }
+
+    // Adds a CodingChallenge to the Educator's list of challenges
+    public void addChallenge(CodingChallenge c){
+        // I am assuming that we will create the challenges ahead of time. If not, this can just be changed to accept no arguments.
+        challenges.add(c);
+    }
+
+    // This is supposed to let the Educator edit the Challenge that they pass in as an argument (???)
+    public void editChallenge(CodingChallenge c){
+        
+    }
+
+    // Since we don't have any code relating to Progress, these methods don't exist
+
+    // Returns the Progress of a given Student
+    // public Progress viewGrades(Student s){
+
+    // }
+
+    // Resets the Progress of a given Student
+    // public void resetStudentProgress(Student s){
+
+    // }
+}
