f5af33a9c5a46c0b1ea0abdf6ba93d4df86b018d
Samantha Aziz <47724926+EpicNotSam@users.noreply.github.com>
2023-12-03 09:00:09 -0500

    Added junit test for create coding challenge
    I added a junit test for create coding challenge, however I couldn't figure out how to make it in the test folder, and have it be able to access the coding challenge class. I fixed it by moving it into the src folder with the other files. The test runs and passes, but it might not be the most elegant setup to have it in src.

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student{
-    String username;
-    String password;
-
-    public Student(String name){
-        username = name;
-        password = "password";
-    }
-}
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    String username;
+    String password;
+
+    public Student(String name){
+        username = name;
+        password = "password";
+    }
+}
