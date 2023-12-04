11a7fe5723c8fcb967bf08cd2c4dd3219173b1af
JAM789-lab <JAM789@pitt.edu>
2023-12-04 18:26:51 -0500

    Allows Students to view challenges from the Main Frame
    Whenever the user chooses the Student option in the Main Frame, they will be prompted to view their challenges. This will display preset CodingChallenges that can be assigned to the Student. Also gave the Student class an ArrayList of CodingChallenges to simulate their progress.

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class Student{
     String username;
     String password;
+    ArrayList<CodingChallenge> progress;
 
     public Student(String name){
         username = name;
         password = "password";
+        progress = new ArrayList<CodingChallenge>();
     }
 }
